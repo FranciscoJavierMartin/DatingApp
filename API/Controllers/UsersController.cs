5e30e7a594b471c91b24d3d99f3985e05a3fab99
Francisco <javiermartincuenca@gmail.com>
2020-12-03 20:14:12 +0100

    LoginDto namescpace does not work

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,15 +7,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class UsersController : ControllerBase
+  public class UsersController : BaseApiController
   {
     private readonly DataContext _context;
     public UsersController(DataContext context)
     {
       _context = context;
-
     }
 
     [HttpGet]
